7d120ef2aeeb0c71b1e50ebf2ede36027b5f293b
user <gamer19@poczta.fm>
2019-12-11 08:18:18 +0100

    update

--- src/main/java/pl/krakow/uek/pp5/creditcard/model/SqlCreditCardStorage.java
+++ src/main/java/pl/krakow/uek/pp5/creditcard/model/SqlCreditCardStorage.java
@@ -1,5 +1,6 @@
 package pl.krakow.uek.pp5.creditcard;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import pl.krakow.uek.pp5.creditcard.model.CreditCard;
 import pl.krakow.uek.pp5.creditcard.model.CreditCardStorage;
 
@@ -7,9 +8,19 @@ import java.util.List;
 
 public class SqlCreditCardStorage implements CreditCardStorage {
 
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public SqlCreditCardStorage(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+
+    }
+
     @Override
     public void add(CreditCard cc) {
-
+        jdbcTemplate.update(
+                "INSERT INTO `cards` (`number`, `limit`, `balance`)" + "VALUES " + "(?,?,?)", cc.getNumber(), cc.getLimit(), cc.getCurrentBalance()
+        );
     }
 
     @Override
